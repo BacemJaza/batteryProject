8afd47c3a3481c2c6c0390a5c3c36382e5fc1ce1
Bacem Jaza <bacemjaza7@gmail.com>
2024-06-22 15:54:40 +0100

    Update navbar.js

--- battery-app/src/layout/navbar.js
+++ battery-app/src/layout/navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Navbar({ onItemClick }) {
   return (
-    <div className="navbar bg-base-100 flex flex-wrap justify-center border-solid border-4 border-[#33333338] bg-[#3B97C6]">
+    <div className="navbar flex flex-wrap justify-center border-solid border-4 border-[#33333338] bg-[#3B97C6]">
       <a href="#general" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("General Information")}>General Information</a>
       <a href="#supply-chain" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("Supply Chain Information")}>Supply Chain Information</a>
       <a href="#material" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("Battery Material and Composition")}>Battery Material and Composition</a>
