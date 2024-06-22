90ea5fddbb96a16525b852a00cb76af038ee7504
Bacem Jaza <bacemjaza7@gmail.com>
2024-06-22 12:26:50 +0100

    update design

--- battery-app/src/layout/Headline.js
+++ battery-app/src/layout/Headline.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 const Headline = () => {
   return (
-    <div className='grid grid-cols-1 lg:grid-cols-2'>
+    <div className='grid grid-cols-1 lg:grid-cols-2 pb-20'>
       <div className='flex justify-center '>
-        <div className='bg-[#0F6D88] absolute h-[50vh] rounded-br-[40%] w-[30%] ml-[-500px] z-[-1] lg:h-[85vh] lg:rounded-br-[20%]'></div>
-      <img src='/images/evBattery.png' width="65%" className='max-h-[300px] mt-[10%] lg:mt-[40%]'/>
+        <div className='bg-[#67C2EE] absolute h-[80vh] rounded-br-[40%] w-[30%] ml-[-500px] z-[-1] lg:h-[95vh] lg:rounded-br-[20%]'></div>
+        <img src='/images/logo.png' className='absolute h-[250px]'/>
+        <img src='/images/evBattery.png' width="65%" className='max-h-[300px] mt-[10%] lg:mt-[40%]'/>
       </div>
       <div className='pt-[5%] max-w-[70%]'>
         <h1 className='text-7xl font-bold pb-20'>
@@ -18,12 +19,6 @@ const Headline = () => {
         <p className='text-2xl font-semibold leading-10'>Explore the essential technical and environmental attributes of the Ultium Battery. Contributing to the broader goals of a greener future.</p>
         <h4 className='text-3xl font-bold'>"Pushing the Limits of Transportation & Technology."</h4>
         <br/>
-        <p>Powered by</p>
-        <br/>
-        <div className='flex max-h-[50px]'>
-        <img src='/images/rcs.png' width="40%"/>
-        <img src='/images/spherity.png' width="30%"/>
-        </div>
         <br/>
         <p>
         Interested in creating and publishing your own verifiable Battery Passport?
@@ -31,8 +26,8 @@ const Headline = () => {
         </p>
         <br/>
         <div className='flex'>
-          <button className='border-solid rounded-lg border-4 mr-2 pl-4 pr-4 pt-1 pb-1 bg-[#023852] text-white hover:bg-white hover:text-black hover:border-[#023852] duration-300'>Get in touch</button>
-          <button className='border-solid rounded-lg border-4 pl-4 pr-4 pt-1 pb-1 bg-[#023852] text-white hover:bg-white hover:text-black hover:border-[#023852] duration-300'>Learn more</button>
+          <button className='border-solid rounded-lg border-4 mr-2 pl-4 pr-4 pt-1 pb-1 bg-[#3B97C6] text-white hover:bg-white hover:text-black hover:border-[#3B97C6] duration-300'>Get in touch</button>
+          <button className='border-solid rounded-lg border-4 pl-4 pr-4 pt-1 pb-1 bg-[#3B97C6] text-white hover:bg-white hover:text-black hover:border-[#3B97C6] duration-300'>Learn more</button>
         </div>
         </div>
         
--- battery-app/src/layout/navbar.js
+++ battery-app/src/layout/navbar.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Navbar({ onItemClick }) {
   return (
-    <div className="navbar bg-base-100 flex flex-wrap justify-center lg:justify-start">
-      <a href="#general" className="btn btn-ghost text-xl" onClick={() => onItemClick("General Information")}>General Information</a>
-      <a href="#supply-chain" className="btn btn-ghost text-xl" onClick={() => onItemClick("Supply Chain Information")}>Supply Chain Information</a>
-      <a href="#material" className="btn btn-ghost text-xl" onClick={() => onItemClick("Battery Material and Composition")}>Battery Material and Composition</a>
-      <a href="#circularity" className="btn btn-ghost text-xl" onClick={() => onItemClick("Design of Circularity")}>Design of Circularity</a>
-      <a href="#performance" className="btn btn-ghost text-xl" onClick={() => onItemClick("Performance and Durability")}>Performance and Durability</a>
+    <div className="navbar bg-base-100 flex flex-wrap justify-center border-solid border-4 border-[#33333338] bg-[#3B97C6]">
+      <a href="#general" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("General Information")}>General Information</a>
+      <a href="#supply-chain" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("Supply Chain Information")}>Supply Chain Information</a>
+      <a href="#material" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("Battery Material and Composition")}>Battery Material and Composition</a>
+      <a href="#circularity" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("Design of Circularity")}>Design of Circularity</a>
+      <a href="#performance" className="btn btn-ghost text-xl text-white" onClick={() => onItemClick("Performance and Durability")}>Performance and Durability</a>
     </div>
   );
 }
